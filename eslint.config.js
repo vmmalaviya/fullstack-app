f96915e287514a6ac954bb855118b19138b23be3
Vishal Malaviya <v.m.malaviya007@gmail.com>
2025-06-15 22:21:38 +0530

    Fix

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,6 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: [globals.node, globals.browser] } },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: { ...globals.node, ...globals.browser } } },
   pluginReact.configs.flat.recommended,
 ]);
